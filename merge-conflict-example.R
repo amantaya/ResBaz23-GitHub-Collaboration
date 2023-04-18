1b0d27435b0438138c2b2394fa075d67303cf140
Andrew Antaya <aantaya@email.arizona.edu>
2023-04-18 11:06:10 -0700

    add merge conflict example

---
+++ merge-conflict-example.R
@@ -0,0 +1,16 @@
+# load and attach packages
+library(datasets)
+library(graphics)
+
+# load example data
+iris <- datasets::iris
+
+# do something with the code
+petal_length <- iris$Petal.Length
+
+petal_width <- iris$Petal.Width
+
+plot(petal_length, petal_width)
+
+# have the other person change this line to pch = 1 and col = "red"
+plot(petal_length, petal_width, pch = 2, col = "blue")

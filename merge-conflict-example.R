3069b427b9d66e26eaa477fae0f3dd3d9ad7efeb
amantaya <aantaya@email.arizona.edu>
2023-04-18 17:52:27 -0700

    add a linear model

--- merge-conflict-example.R
+++ merge-conflict-example.R
@@ -14,3 +14,6 @@ plot(petal_length, petal_width)
 
 # have the other person change this line to pch = 1 and col = "red"
 plot(petal_length, petal_width, pch = 2, col = "purple")
+
+# add a linear model
+lm(petal_length ~ petal_width)

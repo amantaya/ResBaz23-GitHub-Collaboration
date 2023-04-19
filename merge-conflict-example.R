56f14f7b3241133e77a612dede490d53b1fee50b
amantaya <aantaya@email.arizona.edu>
2023-04-18 17:43:45 -0700

    change plot to purple triangles

--- merge-conflict-example.R
+++ merge-conflict-example.R
@@ -13,4 +13,4 @@ petal_width <- iris$Petal.Width
 plot(petal_length, petal_width)
 
 # have the other person change this line to pch = 1 and col = "red"
-plot(petal_length, petal_width, pch = 1, col = "red")
+plot(petal_length, petal_width, pch = 2, col = "purple")
